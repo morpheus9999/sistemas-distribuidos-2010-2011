03229997a0b8057f67c8b55b8e3aa51ab41c5050
morpheus9999 <morpheus9999@52efcf56-b9d5-570b-7529-419c705b8372>
2010-09-28 03:04:24 +0000

    Meti algumas coisas no main e nas classes que o main está a usar

---
+++ Projecto/src/ClientThread.java
@@ -0,0 +1,23 @@
+
+import java.net.Socket;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jorge
+ */
+class ClientThread {
+
+    ClientThread(Socket clientSocket) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    void start() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
