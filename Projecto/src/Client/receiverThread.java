4717ffb3e7dd8f2f648e3f67342e95165108e35c
allumno@gmail.com <allumno@gmail.com@52efcf56-b9d5-570b-7529-419c705b8372>
2010-10-06 17:44:39 +0000

    Alterações na classe Login (nos nomes)
    Criação das classes
    Main
    Menus
    ReceiverThread
    senderThread
    
    com a adição de alguns métodos (muitos deles vazios por dependerem da comunicação que estabelecermos)

---
+++ Projecto/src/Client/receiverThread.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Client;
+
+/**
+ *
+ * @author JLA
+ */
+public class receiverThread extends Thread{
+
+    /*
+     * Constructor
+     */
+    public receiverThread() {
+        super();
+    }
+
+    /*
+     * Run
+     * the main method of the receiver thread
+     */
+    public void run() {
+        
+    }
+}
