f75d76cdc472fb6bf37252baaa34655d25d5e342
allumno@gmail.com <allumno@gmail.com@52efcf56-b9d5-570b-7529-419c705b8372>
2010-10-07 01:37:33 +0000

    O main estava com muita tralha e desorganizado
    Renomeiei a classe menus para Interface
    e coloquei la os metodos referentes aos menus para deixar as coisas mais claras

--- trunk/Projecto/src/Client/Main.java
+++ trunk/Projecto/src/Client/Main.java
@@ -32,71 +32,7 @@ public class Main {
     private static InputStream inStream;
     private static ObjectOutputStream out;
     private static ObjectInputStream in;
-
-
-    /**
-     * Credit
-     * asks for the actual user credit
-     */
-    public void printCredit() {
-
-    }
-
-    /**
-     * Reset Credit
-     * resets the credit of the user
-     */
-    public void resetCredit() {
-
-    }
-
-    /**
-     * Current Matches
-     * lets the user check the matches in play
-     */
-    public void checkMatches() {
-
-    }
-
-    /**
-     * Bet
-     * lets the user bet on any match
-     */
-    public void bet() {
-
-    }
-
-    /**
-     * Online Users
-     * presents the user with a list of online users
-     */
-    public void onlineUsers() {
-
-    }
-
-    /**
-     * Message User
-     * sends a message to a specific user
-     */
-    public void messageSingleUsers() {
-
-    }
-
-    /**
-     * Message All Users
-     * sends a message to all online users
-     */
-    public void messageAllUsers() {
-
-    }
-
-    /**
-     * Logout
-     * drops the connection with the server
-     */
-    public void logout() {
-
-    }
+    
 
     /**
      * Main method
