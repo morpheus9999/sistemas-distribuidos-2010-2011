f01c9d548e5a5cf8eb3193af4b21514f9a2e12bc
allumno@gmail.com <allumno@gmail.com@52efcf56-b9d5-570b-7529-419c705b8372>
2010-10-16 04:22:02 +0000

    OnlineUsers a funcionar
    Mensagens para utilizadores ainda por fazer

---
+++ Projecto/src/Client_Server/OnlineUsers.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Client_Server;
+
+import java.io.Serializable;
+import java.util.Enumeration;
+import java.util.Vector;
+
+/**
+ *
+ * @author JLA
+ */
+public class OnlineUsers implements Serializable{
+    private Vector<String> list = null;
+
+    public OnlineUsers() {
+        this.list = new Vector<String>();
+    }
+
+    public void setList(Vector<String> temp) {
+        this.list = temp;
+    }
+
+    public Vector<String> getList() {
+        return this.list;
+    }
+
+    public void addEntry(String temp) {
+        this.list.add(temp);
+    }
+
+    public void printOnlineUsers() {
+        if(this.list.isEmpty())
+            System.out.println("There are no users online! (is this even possible?)");
+
+        for(int i = 0; i < this.list.size(); i++)
+            System.out.println(">"+this.list.get(i));
+    }
+}
