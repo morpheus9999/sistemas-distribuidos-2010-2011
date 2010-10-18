d37a513c45d01e41d0279a11c7f02c7dfa1d2357
allumno@gmail.com <allumno@gmail.com@52efcf56-b9d5-570b-7529-419c705b8372>
2010-10-18 02:13:01 +0000

    Desculpa a demora Aqui segue o que foi feito ate ao momento
    Reconnect a funcionar como deve ser (podes experimentar para ver se ha algum problema)
    Cliente já não envia objectos repetidos (se enviar, leva nas orelhas)
    etc.

--- trunk/Projecto/src/Client/senderThread.java
+++ trunk/Projecto/src/Client/senderThread.java
@@ -8,7 +8,9 @@ package Client;
 import Client_Server.Constants;
 import Client_Server.Credit;
 import Client_Server.Generic;
+import Client_Server.Login;
 import Client_Server.Message;
+import Client_Server.User;
 import java.io.IOException;
 
 /**
@@ -28,11 +30,15 @@ public class senderThread extends Thread{
      * the main method of the sender thread
      */
     public void run() {
-        try {
-            while(true) {
+        
+        while(!Main.exit) {
+            try {
                 Generic gen = new Generic();
+                Message mes = new Message();
+                Login log = new Login();
+                User reg = new User();
+                
                 int opt = Main.opt.getOption();
-
                 switch(opt) {
                     case Constants.creditCode:
                         /*  get credit balance  */
@@ -67,48 +73,68 @@ public class senderThread extends Thread{
                         break;
                     case Constants.messageCode:
                         /*  messages a user */
-                        gen.setCode(Constants.messageCode);
-                        Message mes = new Message();
                         mes.setHashtable(Main.buffer.getHashtable());
                         mes.setAuthor(Main.buffer.getAuthor());
+
+                        gen.setCode(Constants.messageCode);
                         gen.setObj(mes);
                         break;
                     case Constants.messageAllCode:
                         /*  messages all users  */
+                        mes.setHashtable(Main.bufferAll.getHashtable());
+                        mes.setAuthor(Main.bufferAll.getAuthor());
+                        
                         gen.setCode(Constants.messageAllCode);
-                        gen.setObj(Main.bufferAll);
+                        gen.setObj(mes);
                         break;
                     case Constants.logoutCode:
                         /*  logout and exit */
                         gen.setCode(Constants.logoutCode);
-                        
-                        /*  sends the request to the server */
-                        Main.out.writeObject(gen);
-                        Main.out.flush();
+
+                        /*  only sends if connection is on  */
+                        if(Main.connected) {
+                            /*  sends the request to the server */
+                            Main.out.writeObject(gen);
+                            Main.out.flush();
+                        }
                         /*  ends the thread  */
                         return;
                     case Constants.loginCode:
                         /*  login   */
                         gen.setCode(Constants.loginCode);
-                        gen.setObj(Main.log);
+                        /*  new Login object    */
+                        log.setName(Main.log.getName());
+                        log.setPassword(Main.log.getPassword());
+                        gen.setObj(log);
                         break;
                     case Constants.regCode:
                         /*  register    */
                         gen.setCode(Constants.regCode);
-                        gen.setObj(Main.reg);
+                        /*  new user profile object */
+                        reg.setName(Main.reg.getName());
+                        reg.setMail(Main.reg.getMail());
+                        reg.setPassword(Main.reg.getPassword());
+                        reg.setCredit(Main.reg.getCredit());
+                        gen.setObj(reg);
                         break;
                     default:
                         System.out.println("Wrong option :X");
                         break;
                 }
-                
-                /*  sends the request to the server */
-                Main.out.writeObject(gen);
-                Main.out.flush();
+
+                /*  only sends if connection is on  */
+                if(Main.connected) {
+                    /*  sends the request to the server */
+                    Main.out.writeObject(gen);
+                    Main.out.flush();
+                }
+            } catch (IOException ex) {
+                //  System.out.println("IOException sending object: "+ex.getMessage());
+                //  System.out.println("Error ocurred. Ending sending thread");
             }
-        } catch (IOException ex) {
-            //  System.out.println("IOException sending object: "+ex.getMessage());
-            //  System.out.println("Error ocurred. Ending sending thread");
         }
+        
+
+        System.out.println("sender thread down!!");
     }
 }
