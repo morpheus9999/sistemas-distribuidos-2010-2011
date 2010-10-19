da7620cf6d964cd8bebc682cf41e35cd51edf131
morpheus9999 <morpheus9999@52efcf56-b9d5-570b-7529-419c705b8372>
2010-10-19 04:37:47 +0000

    UUUUUHHHUHHHHUHUHUHUH!!!! ja da para ter 2 servidores e n da bode!!! :P

--- Projecto/src/Server/BetThread.java
+++ Projecto/src/Server/BetThread.java
@@ -21,33 +21,48 @@ class BetThread extends Thread{
 
     int numJogos;
     private int ronda;
+    int tipo;
     BetThread(int numJogos) {
         this.numJogos = numJogos;
-        
+        tipo=0;
     }
 
 
     public void run() {
-         ronda=0;
+         ronda=Queries.rondaActual();
+         tipo=Queries.tipoActual();
 
         while(true){
+            if(tipo==0){
             IBetManager man = new BetManager(numJogos);
+             System.out.println("ENTRA1");
             Queries.NewRound(man, ronda);
-            try {
-                BetThread.sleep(100000);
-            } catch (InterruptedException ex) {
-                System.out.println(ex);
-            }
-            ronda++;
+            tipo++;
+            Queries.actualiza(ronda, 1);
 
+            }else if(tipo==1){
+                try {
+                    long espera =Queries.espera();
+                    System.out.println("ENTRA2"+espera);
+                    if(espera>0)
+                        BetThread.sleep(espera);
+                } catch (InterruptedException ex) {
+                    System.out.println(ex);
+                }
+                ronda++;
+                tipo++;
+            Queries.actualiza(ronda, 2);
+            }else if(tipo==2){
+            
             //Hashtable<String, Generic> envia = new Hashtable();
             System.out.println("Actualiza valores das bets....");
             Queries.updateBets(ronda-1);
+            tipo=0;
             //DELETE jogos
-
+            Queries.actualiza(ronda, 0);
             //Delete apostas
 
-
+            }
         }
         
     }
