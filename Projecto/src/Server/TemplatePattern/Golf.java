14c886d34d3adb9a50efe9bc9bce03f1f94d41f3
Morpheus9999@gmail.com <Morpheus9999@gmail.com@52efcf56-b9d5-570b-7529-419c705b8372>
2011-01-08 18:21:08 +0000

    mais design patterns

---
+++ Projecto/src/Server/TemplatePattern/Golf.java
@@ -0,0 +1,120 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Server.TemplatePattern;
+import BetPackage.BetManager;
+import BetPackage.IBetManager;
+import Client_Server.Message;
+import Server.DAOFactoryPattern.ConsistencyDAO;
+import Server.Main;
+import java.util.Vector;
+import Server.ClientThreadTCP;
+/**
+ *
+ * @author jojo
+ */
+public class Golf extends Game {
+
+    public int numGames;
+    public int ronda;
+    public int tipo;
+    public ConsistencyDAO consistencyDAO;
+
+    public Golf(int numGames, ConsistencyDAO consistencyDAO) {
+        this.numGames = numGames;
+        this.consistencyDAO = consistencyDAO;
+    }
+
+    @Override
+    public void newRoundOfGames() {
+        IBetManager man = new BetManager(numGames);
+        //System.out.println("ENTRA1");
+        consistencyDAO.NewRoundConsistency(man, ronda);
+
+        tipo++;
+        consistencyDAO.actualizaConsistency(ronda, 1);
+
+
+        if (Main.calbackInterfaceTomcat != null) {
+            try {
+                //Main.calbackInterfaceTomcat.printMatches(Queries.viewMatches(new Generic(), ronda));
+                Main.calbackInterfaceTomcat.UpdateMatchs();
+            } catch (Exception ex) {
+                System.out.println("erro callback");
+            }
+
+        }
+
+    }
+
+    @Override
+    public void waitEndRound() {
+        try {
+            long espera = consistencyDAO.esperaConsistency();
+            System.out.println("ACABOU TEMPO DE APOSTAS PARA A RONDA " + ronda);
+            if (espera > 0) {
+                Thread.sleep(espera);
+            }
+        } catch (InterruptedException ex) {
+            System.out.println(ex);
+        }
+        ronda++;
+        tipo++;
+        consistencyDAO.actualizaConsistency(ronda, 2);
+
+    }
+
+    @Override
+    public void endOfRoundGames() {
+        //Hashtable<String, Generic> envia = new Hashtable();
+        System.out.println("Actualiza valores das bets....");
+        Vector<Message> m = consistencyDAO.updateBetsConsistency(ronda - 1);
+        ClientThreadTCP tcp;
+        
+        if (m != null) {
+            for (int k = 0; k < m.size(); k++) {
+
+                tcp = (ClientThreadTCP) (Main.onlineUsersTCP.get(m.elementAt(k).getAuthor()));
+                tcp.messageUser("", m.elementAt(k).getAuthor(), m.elementAt(k).getText());
+                if (Main.calbackInterfaceTomcat != null) {
+                    try {
+                        //Main.calbackInterfaceTomcat.printMatches(Queries.viewMatches(new Generic(), ronda));
+                        Main.calbackInterfaceTomcat.UpdateCredit(m.elementAt(k).getAuthor());
+                    } catch (Exception ex) {
+                        System.out.println("erro callback");
+                    }
+
+                }
+
+            }
+            //DELETE jogos
+            consistencyDAO.actualizaConsistency(ronda, 0);
+
+            //Delete apostas
+
+
+        }
+        tipo = 0;
+
+    }
+
+    @Override
+    public void printWinner() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getRonda() {
+        return ronda;
+    
+    }
+
+    @Override
+    public void getInitialStats() {
+        ronda = consistencyDAO.rondaActualConsistency();
+        tipo = consistencyDAO.tipoActualConsistency();
+    
+    }
+}
